fb92bc55596c13be4546ccd70ea48deff9457024
vaz_mf <vazfernandam@gmail.com>
2021-05-13 12:07:25 -0300

    Estilizando

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text('DeliMeal'),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(15),
         children: DUMMY_CATEGORIES
             .map((categoryData) =>
                 CategoryItem(categoryData.title, categoryData.color))
