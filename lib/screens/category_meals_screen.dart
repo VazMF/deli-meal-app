fe7d6ffad1952e34041701fdc74c2388605903ff
vaz_mf <vazfernandam@gmail.com>
2021-05-13 17:32:05 -0300

    Adicionando imagens

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,3 +1,4 @@
+import '../widgets/meal_item.dart';
 import 'package:flutter/material.dart';
 import '../dummy_data.dart';
 
@@ -20,7 +21,13 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title, 
+            imageUrl: categoryMeals[index].imageUrl, 
+            duration: categoryMeals[index].duration, 
+            complexity: categoryMeals[index].complexity, 
+            affordability: categoryMeals[index].affordability,
+          );
         },
         itemCount: categoryMeals.length,
       ),
