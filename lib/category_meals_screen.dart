7ec641697f0f282ecde9744038ecdc3d60d5e603
vaz_mf <vazfernandam@gmail.com>
2021-05-13 12:47:28 -0300

    Enviando dados via construtor

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+
+  final String categotyId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(this.categotyId, this.categoryTitle);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('The Recipes'),
+        title: Text(categoryTitle),
       ),
       body: Center(
         child: Text('The recipes for the category'),
